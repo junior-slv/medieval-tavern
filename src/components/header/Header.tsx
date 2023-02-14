f84c78f0c35cd121a8fe47d67e4cab38caa10922
junior-slv <dirceu.junior@edu.unifil.br>
2023-02-14 16:17:06 -0300

    initial

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
-
-
 const Header = () => {
+
   return (
-    <div>Header</div>
+    <header className="header">
+        <nav className="nav container">
+            <a href="#home">Taverna 28</a>
+            <div className="nav__menu">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#sobre">Sobre</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#contatos">Contatos</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#onde">Onde nos encontrar</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
   )
 }
 
